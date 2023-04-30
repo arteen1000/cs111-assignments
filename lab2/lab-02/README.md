1b85fc85c2f6934ed69f130b60031ea3603d883c
Arteen Abrishami <arteen1000@gmail.com>
2023-04-29 19:24:26 -0700

    lab 2 pass all unit tests

---
+++ lab2/lab-02/README.md
@@ -0,0 +1,48 @@
+## Round Robin Scheduling Simulation
+
+Program simulates round-robin scheduling for given input file of processes.
+
+## Building
+
+Build the program by running `make` with the provided Makefile.
+
+This program was tested on Linux kernel [release](https://kernel.org) 5.14.8-arch1-1 and also on MacOS Ventura 13.3.1. You can additionally verify this by running `python -m unittest`.
+
+## Documentation of Assumptions
+
+- scheduler will break arrival time ties by list ordering
+- one unit of time is atomic (indivisible)
+- processes produce a response immediately upon execution
+- there is no context switch time
+- the scheduler is "tickless" or "one shot" -- if a process terminates in the middle of a time slice, the next timer interrupt will be generated at `now() + quantum_length`
+- the currently running process will always be queued after those that arrive at the time its quanta is over
+
+## Running the Simulation
+
+# Input File
+
+See the attached file `processes.txt` for an example of the accepted format. The outline is as follows, where any `[---]` must be an unsigned integer:
+
+```
+[number of processes to schedule]
+[first process pid], [first process arrival time], [first process burst time]
+...
+[last process pid], [last process arrival time], [last process burst time]
+
+```
+
+Note that it is not necessary for the list to be ordered by arrival time.
+
+# Executing the Program
+
+Simply perform the following command within your favorite shell:
+
+```
+$ ./rr [path to input file] [time quanta]
+```
+
+and the program will display the average waiting and response times.
+
+## Cleaning up
+
+Clean up all build-related files by running `make clean`.
--- lab2/lab-02/rr.c
+++ lab2/lab-02/rr.c
@@ -4,60 +4,39 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/queue.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-typedef uint32_t u32;
-typedef int32_t i32;
-
-#define DEBUG
-
-/* scheduling asssumptions
- *
- * response time is always instantaneous upon start of exec
- *
- * using one-shot IRQ Timer, reset if processs finish exec early
- * to full quantum for next running process
- *
- * no context switch time
- *
- * break arrival time breaks on list ordering
- *
- * one unit time is indivisible
- */
-
 struct process
 {
-  u32 pid;
-  u32 arrival_time;
-  u32 burst_time;
+  int pid;
+  int arrival_time;
+  int burst_time;
 
   struct process *next;
 
-  u32 remaining_time;
-  u32 start_exec_time;
-  u32 waiting_time;
-  u32 response_time;
-  
+  int remaining_time;
+  int waiting_time;
+  int response_time;
 };
 
-struct process_queue {
+struct {
   struct process *head;
-  struct process *trail; 
+  struct process *tail;
 } sched_queue;
 
-// read in the next unsigned integer from data (can include non-integers)
-u32 next_int(const char **data, const char *data_end)
+struct process *data;
+int size;
+
+int next_int(const char **data, const char *data_end)
 {
-  u32 current = 0;
+  int current = 0;
   bool started = false;
   while (*data != data_end)
   {
     char c = **data;
 
-     // if not a digit
     if (c < 0x30 || c > 0x39)
     {
       if (started)
@@ -69,15 +48,13 @@ u32 next_int(const char **data, const char *data_end)
     {
       if (!started)
       {
-        // digit as an int
         current = (c - 0x30);
         started = true;
       }
       else
       {
-        // get the int correctly if consecutive digits
         current *= 10;
-        current += (c - 0x30); 
+        current += (c - 0x30);
       }
     }
 
@@ -88,12 +65,11 @@ u32 next_int(const char **data, const char *data_end)
   exit(EINVAL);
 }
 
-// C string to integer, exit if bad value (non-digit) anywhere
-u32 next_int_from_c_str(const char *data)
+int next_int_from_c_str(const char *data)
 {
   char c;
-  u32 i = 0;
-  u32 current = 0;
+  int i = 0;
+  int current = 0;
   bool started = false;
   while ((c = data[i++]))
   {
@@ -115,12 +91,10 @@ u32 next_int_from_c_str(const char *data)
   return current;
 }
 
-
 void init_processes(const char *path,
                     struct process **process_data,
-                    u32 *process_size)
+                    int *process_size)
 {
-  // expect path to processes.txt or other to be first argument 
   int fd = open(path, O_RDONLY);
   if (fd == -1)
   {
@@ -129,7 +103,6 @@ void init_processes(const char *path,
     exit(err);
   }
 
-  // return info into 'st' of open file known by 'fd'
   struct stat st;
   if (fstat(fd, &st) == -1)
   {
@@ -138,10 +111,7 @@ void init_processes(const char *path,
     exit(err);
   }
 
-  // file size, in bytes
-  u32 size = st.st_size;
-
-  // map the data from the fd to the region that you return to data_start
+  int size = st.st_size;
   const char *data_start = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
   if (data_start == MAP_FAILED)
   {
@@ -150,15 +120,11 @@ void init_processes(const char *path,
     exit(err);
   }
 
-  // grab address of 'data_start' and 'data_end'
   const char *data_end = data_start + size;
   const char *data = data_start;
 
+  *process_size = next_int(&data, data_end);
 
-  // first int represents numbers of processes to expect
-  *process_size = next_int(&data, data_end); 
-
-  // get enough mem. rep for the # of the processes
   *process_data = calloc(sizeof(struct process), *process_size);
   if (*process_data == NULL)
   {
@@ -167,195 +133,199 @@ void init_processes(const char *path,
     exit(err);
   }
 
-  // get the pid, the arrival time, the burst time for each process in our contig. array
-  for (u32 i = 0; i < *process_size; ++i)
+  for (int i = 0; i < *process_size; ++i)
   {
     (*process_data)[i].pid = next_int(&data, data_end);
     (*process_data)[i].arrival_time = next_int(&data, data_end);
-    (*process_data)[i].remaining_time =
-      (*process_data)[i].burst_time = next_int(&data, data_end);
-
+    (*process_data)[i].remaining_time = (*process_data)[i].burst_time = next_int(&data, data_end);
+    
   }
 
-  // free the mem map and close fd
   munmap((void *)data, size);
   close(fd);
 }
 
-void add_to_sched_queue(struct process *data,
-                        u32 size,
-                        u32 current_arrival_time) {
+bool all_done(void);
 
-  for (u32 i = 0; i < size; i++)
-    {
-    if (data[i].arrival_time == current_arrival_time)
-     {
-     
-      if (!sched_queue.head)
-      {
-        #ifdef DEBUG
-        printf("Process is first in queue: %d\n", data[i].pid);
-        #endif
-        sched_queue.head = sched_queue.trail = &data[i];
-        data[i].next = &data[i];
-        #ifdef DEBUG
-        printf("set next ptr to %p\n", (void*) &data[i]);
-        #endif
-      }
-      else
-        {
-        /* newcomers get last, head is their next */
-        #ifdef DEBUG
-        printf("Process added to queue: %d, %d\n", data[i].pid, i);
-        printf("size, current arrival: %d, %d\n", size, current_arrival_time);
-        #endif
-        data[i].next = sched_queue.head;
-        (sched_queue.trail)->next = &data[i];
-        sched_queue.trail = &data[i];
-        #ifdef DEBUG
-        printf("finished adding to queue\n");
-        #endif
-        }
-      printf("outside else statement\n");
-     }
-    printf("outside if statement\n");
-    }
-  printf("Leaving add_to_sched_queue\n");
+void add_to_sched_queue(int);
+
+void remove_head_from_sched_queue(void);
+
+// #define DEBUG
+#ifdef DEBUG
+void print_out_sched_queue(int current_time) {
+  printf("CURRENT TIME: %d\n", current_time);
+  printf("---------------------\n");
+  printf("sched_queue.head: %p\n", (void*) sched_queue.head);
+  printf("sched_queue.tail: %p\n", (void*) sched_queue.tail);
+  if (sched_queue.head && sched_queue.tail) {
+  struct process *trav = sched_queue.head;
+  do {
+    printf("pid: %d\n", trav->pid);
+    printf("arrival_time: %d\n", trav->arrival_time);
+    printf("burst_time: %d\n", trav->burst_time);
+    printf("next: %p\n", (void*) trav->next);
+    printf("cur: %p\n", (void*) trav);
+    printf("remaining_time: %d\n", trav->remaining_time);
+    printf("waiting_time: %d\n", trav->waiting_time);
+    printf("response_time: %d\n", trav->response_time);
+    trav = trav->next;
+  } while (trav != sched_queue.head);
+  printf("------\n");
+  }
 }
 
-void remove_head_from_sched_queue() {
-  if (!sched_queue.head) return;
-  #ifdef DEBUG
-  printf("Removing head from queue\n");
-  #endif
-  if (sched_queue.head == sched_queue.trail) {
-    sched_queue.head = sched_queue.trail = NULL;
-    return;
-  }
-  
-  sched_queue.head = (sched_queue.head)->next;
-  (sched_queue.trail)->next = sched_queue.head;
+void print_out_data(void) {
+  printf("ALL DATA\n");
+  printf("---------------------\n");
+  int i = 0;
+  struct process *trav = &data[i];
+  do {
+    printf("pid: %d\n", trav->pid);
+    printf("-------\n");    
+    printf("arrival_time: %d\n", trav->arrival_time);
+    printf("burst_time: %d\n", trav->burst_time);
+    printf("next: %p\n", (void*) trav->next);
+    printf("cur: %p\n", (void*) trav);
+    printf("remaining_time: %d\n", trav->remaining_time);
+    printf("waiting_time: %d\n", trav->waiting_time);
+    printf("response_time: %d\n", trav->response_time);
+    trav = &data[++i];
+  } while (trav != &data[size]);
+  printf("---------------------\n");
 }
 
-bool done_scheduling(struct process *data, u32 size) {
-  for (u32 i = 0; i < size; i++) {
-    if (data[i].remaining_time != 0) {
-      #ifdef DEBUGE
-      printf("Process with PID: %d has time left\n", data[i].pid);
-      #endif
-      return false;
-    }
-  }
-  #ifdef DEBUG
-  printf("Done scheduling\n");
-  #endif
-  return true;
-}
+#endif
 
-void increment_waits(u32 amnt) {
-  struct process *trav;
-  trav = (sched_queue.head)->next;
-  while (trav != sched_queue.head) {
-    trav->waiting_time += amnt;
-  }
-}
-u32 run_current_head(u32 quantum_length, u32 current_time) {
-  if(!sched_queue.head || !sched_queue.trail) return 0;
-  if ((sched_queue.head)->remaining_time == (sched_queue.head)->burst_time) {
-    (sched_queue.head)->start_exec_time = current_time;
-    (sched_queue.head)->response_time = current_time - (sched_queue.head)->arrival_time;
-  }
-  u32 val;
-  if ((val = (sched_queue.head)->remaining_time) < quantum_length) {
-    (sched_queue.head)->remaining_time = 0;
-    increment_waits(val);
-    remove_head_from_sched_queue();
-    return val;
-  } else if ((sched_queue.head)->remaining_time == quantum_length) {
-    (sched_queue.head)->remaining_time = 0;
-    increment_waits(quantum_length);
-    remove_head_from_sched_queue();
-    return quantum_length;
-  } else {
-    #ifdef DEBUG
-    printf("Decreasing by quantum length, >");
-    #endif
-    (sched_queue.head)->remaining_time -= quantum_length;
-    increment_waits(quantum_length);
-    return quantum_length;
-  }
-}
+
+
+void increment_waits(void);
 
 int main(int argc, char *argv[])
 {
-  // req. 3 args
   if (argc != 3)
   {
     return EINVAL;
   }
-
-  // init process data and size from first argument
-  struct process *data;
-  u32 size;
+  
   init_processes(argv[1], &data, &size);
 
-  // get quantum length from second argument
-  u32 quantum_length = next_int_from_c_str(argv[2]);
+  int quantum_length = next_int_from_c_str(argv[2]);
+
+  int total_waiting_time = 0;
+  int total_response_time = 0;
+
+  int current_time = 0;
+  int curr_run = 0;
+
+  add_to_sched_queue(current_time);
   
+  do {
+    if (all_done()) break;
+    
+    if (sched_queue.head)
+      {
 
-  // init values to increment and return
-  u32 total_waiting_time = 0;
-  u32 total_response_time = 0;
+      if (sched_queue.head->remaining_time == sched_queue.head->burst_time)
+        sched_queue.head->response_time = current_time - sched_queue.head->arrival_time;
+      
+      sched_queue.head->remaining_time--;
+      curr_run++;
+      
+#ifdef SAFE
+      if (sched_queue.head->remaining_time < 0) {
+        fprintf(stderr, "remaining_time < 0");
+        exit(1);
+      }
+#endif
 
-  u32 i = 0;
-  u32 j = 0;
-  bool run_event = true;
-  do
-  {
-    #ifdef DEBUG
-    printf("iteration: %d\n", i);
-    #endif
-    if (done_scheduling(data,size)) break;
-    add_to_sched_queue(data, size, i);
-    #ifdef DEBUG
-    printf("Leaving add_to_sched_queue");
-    #endif
+      increment_waits();
+      add_to_sched_queue(++current_time);
+      
+      if (sched_queue.head->remaining_time == 0) {
+        curr_run = 0;
+        remove_head_from_sched_queue();
+      } else if (curr_run == quantum_length) {
+        curr_run = 0;
+        if ( sched_queue.head != sched_queue.tail ) {
+          sched_queue.head = sched_queue.head->next;
+          sched_queue.tail = sched_queue.tail->next;
+        }
+      }
+      } else add_to_sched_queue(++current_time);
+    
+#ifdef DEBUG
+    print_out_sched_queue(current_time);
+#endif    
+  } while (true);
 
-    if (run_event) {
-      #ifdef DEBUG
-      printf("Entering run_event\n");
-      #endif
-      j = run_current_head(quantum_length, i);
-      j += i;
-      run_event = false;
-    }
-    if (i == j) {
-      run_event = true;
-      sched_queue.head = (sched_queue.head)->next;
-      sched_queue.trail = (sched_queue.trail)->next;
-    }
-
-    #ifdef DEBUG
-    struct process *trav = sched_queue.head;
-    do {
-      printf("PID: %d\n", trav->pid);
-      printf("Remaining time: %d\n", trav->remaining_time);
-      printf("Start exec time: %d\n", trav->start_exec_time);
-      printf("Waiting time: %d\n", trav->waiting_time);
-      printf("Response time: %d\n", trav->response_time);
-      printf("Next ptr: %p\n", (void*) trav->next);
-      printf("Self address: %p\n", (void*) trav);
-      trav = trav->next;
-    } while(trav != sched_queue.head);
-    printf("Time unit: %d\n", i);
-    printf("run event next cycle: %d\n", run_event);
-    #endif
-  } while (++i);
-  
-
-  printf("Average waiting time: %.2f\n", (float)total_waiting_time / (float)size);
-  printf("Average response time: %.2f\n", (float)total_response_time / (float)size);
+  for (int i = 0 ; i < size ; i++) {
+    total_response_time += data[i].response_time;
+    total_waiting_time += data[i].waiting_time;
+  }
+#ifdef DEBUG
+  print_out_data();
+#endif  
+  printf("Average waiting time: %.2f\n", (float) total_waiting_time / size);
+  printf("Average response time: %.2f\n", (float) total_response_time / size);
 
   free(data);
   return 0;
 }
+
+void increment_waits(void) {
+  struct process *trav = sched_queue.head->next;
+  while (trav != sched_queue.head) {
+    trav->waiting_time++;
+    trav = trav->next;
+  }
+}
+
+bool all_done(void) {
+  for (int i = 0 ; i < size ; i++) {
+    if (data[i].remaining_time > 0) return false;
+  }
+  return true;
+}
+
+void add_to_sched_queue(int current_time) {
+
+  for (int i = 0 ; i < size ; i++) {
+
+    if (data[i].arrival_time == current_time) {
+
+      if (sched_queue.head == NULL) {
+        sched_queue.head = sched_queue.tail = &data[i];
+        data[i].next = &data[i];
+      }
+      else {
+        data[i].next = sched_queue.head;
+        sched_queue.tail->next =  &data[i];
+        sched_queue.tail = &data[i];
+      }
+    }
+  }
+}
+
+void remove_head_from_sched_queue(void) {
+#ifdef SAFE  
+  if (!sched_queue.head || !sched_queue.tail) {
+    fprintf(stderr, "trying to remove empty head");
+    exit(1);
+  }
+#endif  
+  if (sched_queue.head == sched_queue.tail) {
+    sched_queue.head = sched_queue.tail = NULL;
+    return;
+  }
+
+  sched_queue.head = sched_queue.head->next;
+  sched_queue.tail->next = sched_queue.head;
+  
+}
+
+
+        
+  
+  
+  
